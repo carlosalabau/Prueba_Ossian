dc1cf9500756bdd64b679cecd71ac85233465819
Carlos Alabau <carlytos347@gmail.com>
2020-07-28 16:21:54 +0200

    Mas test y mejoras front

--- backend/tests/Feature/imagesTest.php
+++ backend/tests/Feature/imagesTest.php
@@ -13,15 +13,31 @@ class imagesTest extends TestCase
      *
      * @return void
      */
-    public function testExample()
+    public function addImage()
     {
         $response = $this->json('POST','api/image/add',
         [
+            'id'=>9,
             'title'=>'Prueba',
             'description'=>'Prueba 1',
             'category'=>'Informatica',
             'url'=>'prueba'
         ]);
         $response->assertStatus(201);    
+    }
+    public function detailImage(){
+        $response = $this->get('api/image/detail/9');
+        $response->assertSuccessful();
+    }
+    public function deleteImage(){
+        $response = $this->delete('api/image/delete/9');
+        $response->assertSuccessful();
+    }
+    public function updateImage(){
+        $response = $this->json('PUT', 'api/image/update/9',
+        [
+            'title'=>'Prueba tecnica'
+        ]);
+        $response->assertSuccessful();
     }
 }
